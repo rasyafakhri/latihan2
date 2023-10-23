23c723a02b0c0d598df786fae265d9745d9efd30
amur <rasyafakhri.akbar@gmail.com>
2023-10-23 12:34:05 +0700

    menambahkan file python lat3.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # latihan2
+## dikerjakan oleh rasya fakhri akbar
+### python
---
+++ lat3.py
@@ -0,0 +1,7 @@
+print("hello")
+print("saya sedang belajar python")
+a=5
+b=10
+print("varibel a=",a)
+print("varibel b=",b)
+print("hasil penjumlahan a+b=",a+b)
